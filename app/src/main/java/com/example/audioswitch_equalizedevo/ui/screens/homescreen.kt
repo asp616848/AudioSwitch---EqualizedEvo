b806012410fa4f364e082c84e460c87e5c7ebfc4
asp616848 <asp616848@gmail.com>
2024-04-07 12:04:38 +0530

    HomeScreen started

--- app/src/main/java/com/example/audioswitch_equalizedevo/ui/screens/homescreen.kt
+++ app/src/main/java/com/example/audioswitch_equalizedevo/ui/screens/homescreen.kt
@@ -1,8 +1,70 @@
 package com.example.audioswitch_equalizedevo.ui.screens
 
+import androidx.compose.foundation.layout.PaddingValues
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
+import androidx.compose.material.icons.filled.Favorite
+import androidx.compose.material.icons.filled.Menu
+import androidx.compose.material3.CenterAlignedTopAppBar
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.material3.TopAppBar
+import androidx.compose.material3.TopAppBarDefaults
+import androidx.compose.material3.rememberTopAppBarState
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.input.nestedscroll.nestedScroll
+import androidx.compose.ui.text.style.TextOverflow
+import com.example.audioswitch_equalizedevo.R
 
+@OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun HomeScreen() {
+    val scrollBehavior = TopAppBarDefaults.pinnedScrollBehavior(rememberTopAppBarState())
+    Scaffold(
+        modifier = Modifier.nestedScroll(scrollBehavior.nestedScrollConnection),
 
+        topBar = {
+            CenterAlignedTopAppBar(
+                title = { Text(
+                            text = "Music Player",
+                            maxLines = 1,
+                            overflow = TextOverflow.Ellipsis)
+                        },
+                colors = TopAppBarDefaults.topAppBarColors(
+                    containerColor = MaterialTheme.colorScheme.onPrimary,
+                    titleContentColor = MaterialTheme.colorScheme.onPrimary,
+                    ),
+                navigationIcon = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Menu,
+                            contentDescription = "Menu Bar"
+                        )
+                    }
+                },
+                actions = {
+                    IconButton(onClick = { /* do something */ }) {
+                        Icon(
+                            imageVector = Icons.Filled.Favorite,
+                            contentDescription = "Liked Songs"
+                        )
+                    }
+                },
+                scrollBehavior = scrollBehavior,
+            )
+        }
+    ) {
+        SongsScreen(it)
+    }
+}
+
+@Composable
+fun SongsScreen(it: PaddingValues) {
+    Text(text = "Songs Screen", modifier = Modifier.padding(it))
 }
