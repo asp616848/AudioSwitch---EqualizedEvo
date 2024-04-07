746a2aa36f8b5b28bd3628887a6b8f6a9e85aee4
asp616848 <asp616848@gmail.com>
2024-04-07 15:00:44 +0530

    update

---
+++ app/src/main/java/com/example/audioswitch_equalizedevo/data/UIState.kt
@@ -0,0 +1,5 @@
+package com.example.audioswitch_equalizedevo.data
+
+data class UIState(val title:String = " ") {
+
+}
---
+++ app/src/main/java/com/example/audioswitch_equalizedevo/ui/viewModels/viewModel.kt
@@ -0,0 +1,14 @@
+package com.example.audioswitch_equalizedevo.ui.viewModels
+
+import androidx.lifecycle.ViewModel
+import com.example.audioswitch_equalizedevo.data.UIState
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.flow.asStateFlow
+
+//taken viewModel inspiration from my scramble codelab project
+class viewModel : ViewModel() {
+    private val _uiState = MutableStateFlow(UIState())
+    val uiState: StateFlow<UIState> = _uiState.asStateFlow()
+    
+}
