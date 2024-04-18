47f1d508c4dfb17afafb3b2a7a23e4febfc8a056
asp616848 <asp616848@gmail.com>
2024-04-18 06:50:02 +0530

    Update PlaybackActitivy.kt

--- app/src/main/java/com/example/audioswitch_equalizedevo/ui/activities/PlaybackActitivy.kt
+++ app/src/main/java/com/example/audioswitch_equalizedevo/ui/activities/PlaybackActitivy.kt
@@ -1,4 +1,10 @@
 package com.example.audioswitch_equalizedevo.ui.activities
 
-class PlaybackActitivy {
+import androidx.activity.ComponentActivity
+import androidx.media3.exoplayer.ExoPlayer
+import dagger.Component
+
+class PlaybackActitivy : ComponentActivity(){
+    val player = ExoPlayer.Builder(this).build()
+    
 }
