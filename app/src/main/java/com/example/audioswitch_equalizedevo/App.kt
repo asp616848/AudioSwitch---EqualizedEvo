ca85c5c004686178d6d562c2abcdf749e2ac73c6
asp616848 <asp616848@gmail.com>
2024-04-20 20:06:13 +0530

    working with hild

--- app/src/main/java/com/example/audioswitch_equalizedevo/App.kt
+++ app/src/main/java/com/example/audioswitch_equalizedevo/App.kt
@@ -1,5 +1,12 @@
 package com.example.audioswitch_equalizedevo
-//application class
 
-class App {
+import android.app.Application
+import dagger.hilt.android.HiltAndroidApp
+
+@HiltAndroidApp
+class App:Application() {
+    override fun onCreate() {
+        super.onCreate()
+
+    }
 }
